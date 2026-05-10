309aae33623092e7758c47957b0879c409505a29
declan712 <declan712@gmail.com>
2026-05-11 09:39:31 +1000

    candy right click now affects player

--- src/main/java/dev/hytalemodding/interactions/SetCreatureNameplateInteraction.java
+++ src/main/java/dev/hytalemodding/interactions/SetCreatureNameplateInteraction.java
@@ -3,6 +3,7 @@ package dev.hytalemodding.interactions;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import com.hypixel.hytale.assetstore.map.IndexedLookupTableAssetMap;
 import com.hypixel.hytale.codec.builder.BuilderCodec;
 import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.Ref;
@@ -11,7 +12,11 @@ import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.protocol.InteractionState;
 import com.hypixel.hytale.protocol.InteractionType;
 import com.hypixel.hytale.server.core.entity.InteractionContext;
+import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.entity.nameplate.Nameplate;
+import com.hypixel.hytale.server.core.modules.entitystats.EntityStatMap;
+import com.hypixel.hytale.server.core.modules.entitystats.EntityStatValue;
+import com.hypixel.hytale.server.core.modules.entitystats.asset.EntityStatType;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.CooldownHandler;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.config.SimpleInstantInteraction;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
@@ -21,8 +26,6 @@ import dev.hytalemodding.components.PkmnCaptureMetadata;
 import dev.hytalemodding.components.PkmnStatsComponent;
 import dev.hytalemodding.util.PkmnStatUtils;
 
-
-//TODO: clean up, use shared logic, fix lvl1 bug
 
 public class SetCreatureNameplateInteraction extends SimpleInstantInteraction {
     public static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
@@ -49,7 +52,36 @@ public class SetCreatureNameplateInteraction extends SimpleInstantInteraction {
             fail(interactionContext);
             return;
         }
-        NPCEntity npcEntity = store.getComponent(ref,NPCEntity.getComponentType());
+        Player player = commandBuffer.getComponent(ref, Player.getComponentType());
+        if(player != null){
+            LOGGER.atInfo().log("Running for player");
+            // triggered by player level up, not naming NPC
+            PkmnStatsComponent pkmnStats = commandBuffer.getComponent(ref, PkmnStatsComponent.getComponentType());
+            if (pkmnStats == null) {
+                LOGGER.atInfo().log("Current stats NULL");
+                pkmnStats = new PkmnStatsComponent();
+            }
+            if(pkmnStats.getBaseStats() != PkmnStatUtils.PLAYER_BASE_STATS){ 
+                LOGGER.atInfo().log("Setting base stats");
+                pkmnStats.setBaseStats(PkmnStatUtils.PLAYER_BASE_STATS);
+            }
+            EntityStatMap stats = store.getComponent(ref, EntityStatMap.getComponentType());
+            IndexedLookupTableAssetMap<String, EntityStatType> assetMap = EntityStatType.getAssetMap();
+            int lvlIdx = assetMap.getIndex("Lvl");
+            int expIdx = assetMap.getIndex("Exp");
+            EntityStatValue lvl = stats.get(lvlIdx);
+            EntityStatValue exp = stats.get(expIdx);
+            float currentExp = exp.get();
+            float currentLvl = lvl.get();
+            pkmnStats.setLevel((int)currentLvl);
+            pkmnStats.setExperience((int)currentExp);
+            PkmnStatUtils.apply(store,commandBuffer,ref,pkmnStats);
+            commandBuffer.putComponent(ref, PkmnStatsComponent.getComponentType(), pkmnStats);
+            next(interactionContext);
+            return; 
+        }
+
+        NPCEntity npcEntity = commandBuffer.getComponent(ref,NPCEntity.getComponentType());
         if(npcEntity==null){
             fail(interactionContext);
             return;
